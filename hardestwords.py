cc32d48e327de254c308fee6e2d8bc0d6d8f500f
kryptonhexafluoride <hi2meera@gmail.com>
2025-04-23 15:44:40 -0700

    initial commit

---
+++ hardestwords.py
@@ -0,0 +1,14 @@
+# parse in word list
+words = set()
+words_file = open("dictionary.txt", "r")
+for word in words_file.readlines():
+    word = word.strip();
+    words.add(word)
+
+# rank frequencies of each letter in each spot
+frequencies = {}
+
+# draw a graph 
+
+# loop through the word list to see which has the lowest popularity rating
+
